1449c5cbcbfcc0509cea6e0bbd603cfad98327b3
Spliterash <me@spliterash.ru>
2022-07-21 11:24:43 +0300

    Отписывание листенеров при закрытии context'а спринга

--- src/main/java/ru/spliterash/springspigot/listener/SpigotListenerInitializer.java
+++ src/main/java/ru/spliterash/springspigot/listener/SpigotListenerInitializer.java
@@ -6,15 +6,16 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.DestructionAwareBeanPostProcessor;
 import org.springframework.context.annotation.Configuration;
 import ru.spliterash.springspigot.utils.ProxyUtils;
 
 import javax.annotation.PreDestroy;
+import java.util.Optional;
 
 @Configuration
 @RequiredArgsConstructor
-public class SpigotListenerInitializer implements BeanPostProcessor {
+public class SpigotListenerInitializer implements DestructionAwareBeanPostProcessor {
     private final JavaPlugin plugin;
 
     @PreDestroy
@@ -22,16 +23,30 @@ public class SpigotListenerInitializer implements BeanPostProcessor {
         HandlerList.unregisterAll(plugin);
     }
 
-    @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+    private Optional<Listener> getListener(Object bean) {
         if (bean instanceof Listener) {
             Class<?> targetClass = AopUtils.getTargetClass(bean);
             if (targetClass.isAnnotationPresent(SpigotListener.class)) {
                 Listener realListener = ProxyUtils.getProxyTarget(bean);
-                plugin.getServer().getPluginManager().registerEvents(realListener, plugin);
+
+                return Optional.of(realListener);
             }
         }
 
+        return Optional.empty();
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        getListener(bean)
+                .ifPresent(l -> plugin.getServer().getPluginManager().registerEvents(l, plugin));
+
         return bean;
+    }
+
+    @Override
+    public void postProcessBeforeDestruction(Object bean, String beanName) throws BeansException {
+        getListener(bean)
+                .ifPresent(HandlerList::unregisterAll);
     }
 }
