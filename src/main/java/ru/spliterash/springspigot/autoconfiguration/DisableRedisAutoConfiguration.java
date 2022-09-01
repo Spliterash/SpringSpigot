2b1cc9e4049d3b095b2df1ecd9c1bab0f25dda06
Spliterash <me@spliterash.ru>
2022-09-01 18:33:48 +0300

    Добавил disable redis om

--- src/main/java/ru/spliterash/springspigot/autoconfiguration/DisableRedisAutoConfiguration.java
+++ src/main/java/ru/spliterash/springspigot/autoconfiguration/DisableRedisAutoConfiguration.java
@@ -1,18 +1,12 @@
 package ru.spliterash.springspigot.autoconfiguration;
 
+import com.redis.om.spring.RedisModulesConfiguration;
 import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -20,7 +14,8 @@ import org.springframework.context.annotation.Configuration;
         RedisAutoConfiguration.class,
         RedisRepositoriesAutoConfiguration.class,
         RedisReactiveAutoConfiguration.class,
-        RedissonAutoConfiguration.class
+        RedissonAutoConfiguration.class,
+        RedisModulesConfiguration.class,
 })
 @ConditionalOnExpression("${spring-spigot.db.redis:false} == false")
 class DisableRedisAutoConfiguration {
