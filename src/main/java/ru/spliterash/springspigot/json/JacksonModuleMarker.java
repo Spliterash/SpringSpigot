5ef9d8a9c632da9b7c2cb8721673da4f80d1b6fb
Spliterash <me@spliterash.ru>
2022-12-14 18:53:02 +0300

    Добавил поддержку старых маркеров для jackson

---
+++ src/main/java/ru/spliterash/springspigot/json/JacksonModuleMarker.java
@@ -0,0 +1,8 @@
+package ru.spliterash.springspigot.json;
+
+/**
+ * Класс, предоставляющий какой-либо модуль для objectmapper'а
+ */
+@Deprecated
+public interface JacksonModuleMarker {
+}
--- src/main/java/ru/spliterash/springspigot/json/ObjectMapperService.java
+++ src/main/java/ru/spliterash/springspigot/json/ObjectMapperService.java
@@ -10,21 +10,27 @@ import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 
+@SuppressWarnings("deprecation")
 @Log4j2
 @Component
 @RequiredArgsConstructor
 public class ObjectMapperService {
     private final Collection<JacksonModuleProvider> modules;
+    private final Collection<JacksonModuleMarker> deprecatedDefinitionModules;
 
     public void configureObjectMapper(ObjectMapper mapper) {
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
+        for (JacksonModuleMarker module : deprecatedDefinitionModules) {
+            if (module instanceof Module)
+                mapper.registerModule((Module) module);
+        }
 
-        for (JacksonModuleProvider module : (modules)) {
+        for (JacksonModuleProvider module : modules) {
             Module jacksonModule = module.getModule();
             if (jacksonModule != null)
-                mapper.registerModule((Module) module);
+                mapper.registerModule(jacksonModule);
         }
     }
 }
