4f37058dd62237e932a66aa9f0ad6f88bb0f550f
MelonHell <MelonHell@ya.ru>
2023-03-24 16:55:44 +0300

    фикс загрузки при отсутствии kotlinmc

--- src/main/java/ru/spliterash/springspigot/json/modules/KotlinSerializer.java
+++ src/main/java/ru/spliterash/springspigot/json/modules/KotlinSerializer.java
@@ -2,10 +2,12 @@ package ru.spliterash.springspigot.json.modules;
 
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.module.kotlin.KotlinModule;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.stereotype.Component;
 import ru.spliterash.springspigot.json.JacksonModuleProvider;
 
 @Component
+@ConditionalOnClass(name = "ru.spliterash.kotlinmc.CoroutinePlugin")
 public class KotlinSerializer implements JacksonModuleProvider {
     @Override
     public Module getModule() {
